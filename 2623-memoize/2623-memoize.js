301c07feecf637808eae78ea2b85424c1cfeb862
Ziyaurrahman <ziyaurrahman457@gmail.com>
2026-08-17 22:35:36 +0530

    Time: 266 ms (44.83%), Space: 94.3 MB (76.08%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache={}
+    return function(...args) {
+        // let n=args[0];
+        let n = JSON.stringify(args)
+        if(n in cache){
+            return cache[n];
+        } else{
+            // let result=fn(n);
+            let result = fn.apply(this, args);
+            cache[n]=result;
+            return result;
+
+
+        }
+        
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
